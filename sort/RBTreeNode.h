0876eaeff145aa890b7c501b3649d145577720c2
messi <chengyueww@gmail.com>
2021-05-15 15:33:06 +0800

    添加RBTree及RBTreeNode类

---
+++ sort/RBTreeNode.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstring>
+
+template <class T>
+class RBTreeNode{
+    private:
+        T val;
+        RBTreeNode *root;
+
+};
