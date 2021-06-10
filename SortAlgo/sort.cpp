33e754385fb06a3972170aa524c6390c1a4d58b2
messi <chengyueww@gmail.com>
2021-06-10 22:43:25 +0800

    增加sort目录
    排序

---
+++ SortAlgo/sort.cpp
@@ -0,0 +1,30 @@
+//#include <cstdio>
+#include <ctime>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#define MAX_NUM 20
+
+int partition(vector<int> &array, int low, int high) {
+  int p = low;
+  return p;
+}
+void print_vector(vector<int> array) {
+  for (auto iter = array.begin(); iter != array.end(); iter++) {
+    cout << *iter << " ";
+  }
+  cout << endl;
+}
+int main() {
+  vector<int> array;
+  srand(time(nullptr));
+
+  for (int i = 0; i < MAX_NUM; i++) {
+    int j = rand() % MAX_NUM;
+    array.push_back(j);
+  }
+  print_vector(array);
+  return 0;
+}
