e23c6c5a721cd8a72bc71fcbde2963dff70e2fd9
messi <chengyueww@gmail.com>
2021-06-24 23:37:28 +0800

    修改vscode配置

--- etc/util.h
+++ etc/util.h
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <cmath>
 #include <cstdio>
 #include <cstdlib>
@@ -7,6 +8,9 @@
 #include <iostream>
 #include <map>
 #include <set>
+#include <stack>
+#include <string>
 #include <unordered_map>
 #include <unordered_set>
+#include <vector>
 using namespace std;
